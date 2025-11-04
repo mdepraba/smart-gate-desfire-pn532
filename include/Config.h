613e2aba8a53a990bef30e240020135a0bdf00ab
mdepraba <prabakusuma764@gmail.com>
2025-11-05 00:41:14 +0800

    Init new sketch, change pn532 test to nfc, and add dependencies for smart gate
    Moved NFC and Desfire card handling logic from smart-gate-sketch.cpp to a new src/nfc.cpp file. Updated platformio.ini to add PubSubClient, ESP32Servo, and ArduinoJson libraries. Expanded Config.h with pin definitions and credentials for Wi-Fi and MQTT. Prepared smart-gate-sketch.cpp for integration with Wi-Fi, MQTT, and servo control by including relevant libraries and initializing PubSubClient.

--- include/Config.h
+++ include/Config.h
@@ -1,6 +1,23 @@
 #pragma once
 
+// PN532 Pins
 #define PN532_SS   5
 #define PN532_RST  21
 #define LED_PIN    2
 #define USE_AES    true     // true = AES, false = 3DES
+
+// Servo Pin
+#define SERVO_PIN  32
+
+// Ultrasonic Sensor Pins
+#define TRIG_PIN   16
+#define ECHO_PIN   17
+
+// Wi-Fi Credentials
+const char* ssid = "LIT-2.4G-CSSU";
+const char* password = "yakaligaarka2.4G";
+
+// MQTT Credentials
+const char* mqtt_server = "192.168.18.5";
+const char* mqtt_user = "arkalit";
+const char* mqtt_pass = "arkamqtt";
